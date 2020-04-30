bc48be9072b7f2ffb36b4cb87a90e08a1d7df9ba
youmeSon <rosydarling@naver.com>
2020-04-30 12:18:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function milesToKm(distance) {
+    return distance * 1.60934;
+}
+
+function kmToMiles(distance) {
+    return distance / 1.60934;
+}
+
+document.addEventListener('submit', e => {
+    e.preventDefault();
+    let distanceEntered = document.querySelector('#distance-input').value;
+
+    let convertedDistance;
+
+    if (document.querySelector('#mToKm').checked){
+        convertedDistance = milesToKm(distanceEntered);
+    } else {
+        convertedDistance = kmToMiles(distanceEntered);
+    }
+
+    document.querySelector('#distance-result').innerText = convertedDistance;
+    
+    document.querySelector('.result-div').classList.remove('d-none');
+
+    document.querySelector('#distance-input').value =
+});
